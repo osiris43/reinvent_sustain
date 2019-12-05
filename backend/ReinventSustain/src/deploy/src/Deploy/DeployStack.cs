77e62d3e26dd6767481c974fe9e601de3e93cdd2
Brett Bim <osiris43@gmail.com>
2019-12-05 15:42:21 -0800

    deploy

---
+++ backend/ReinventSustain/src/deploy/src/Deploy/DeployStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace Deploy
+{
+    public class DeployStack : Stack
+    {
+        public DeployStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
