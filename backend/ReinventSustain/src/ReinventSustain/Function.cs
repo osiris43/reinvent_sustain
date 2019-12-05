d9e91284fbf2001cfe26424d3a3c6adff647645a
Chuck Terrell <cterrell@onetechnologies.net>
2019-12-05 15:15:28 -0800

    WIP

--- backend/ReinventSustain/src/ReinventSustain/Function.cs
+++ backend/ReinventSustain/src/ReinventSustain/Function.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 using Amazon.Lambda.Core;
 
@@ -12,6 +14,12 @@ namespace ReinventSustain
 {
     public class Function
     {
+
+        private const string bucketName = "otbirds";
+        private const string keyName = "birddatajson";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+        private static IAmazonS3 client;
         
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
@@ -21,7 +29,42 @@ namespace ReinventSustain
         /// <returns></returns>
         public string FunctionHandler(string input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            client = new AmazonS3Client(bucketRegion);
+            var results = await ReadObjectDataAsync().Wait();
+
+            return results;
         }
+
+        static async Task<string> ReadObjectDataAsync()
+        {
+            string responseBody = "";
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+                using (GetObjectResponse response = await client.GetObjectAsync(request))
+                using (Stream responseStream = response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    Console.WriteLine("Content type: {0}", contentType);
+                    Console.WriteLine("Received bird data");
+
+                    responseBody = reader.ReadToEnd(); // Now you process the response body.
+                }
+
+                return responseBody;
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered ***. Message:'{0}' when reading an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when reading an object", e.Message);
+            }
+        } 
     }
 }
